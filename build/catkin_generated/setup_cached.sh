3cd5c0cf48c030afa26add95ac5a8cd18ff43af7
deansun <deansun69@gmail.com>
2021-09-06 14:53:42 +0800

    add lesson3 pratice add C_three.cpp

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/dean/test/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/dean/test/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/dean/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/dean/test/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/dean/test/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/dean/test/src:$ROS_PACKAGE_PATH"
---
+++ build/example/CMakeFiles/C_three.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_three.dir/src/C_two.cpp.o"
+  "/home/dean/test/devel/lib/example/C_three.pdb"
+  "/home/dean/test/devel/lib/example/C_three"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_three.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
